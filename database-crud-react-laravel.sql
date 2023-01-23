7213c0db558fd9ffdbddf8a4460eebda7b9453c2
Barbara Teresa <barbara-teresa@outlook.com>
2023-01-23 11:08:03 -0300

    Add script de criação do banco de dados

---
+++ database-crud-react-laravel.sql
@@ -0,0 +1,10 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.0.30 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.1.0.6537
+-- --------------------------------------------------------
+
+-- Copiando estrutura do banco de dados para crud_react_laravel
+CREATE DATABASE IF NOT EXISTS `crud_react_laravel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `crud_react_laravel`;
